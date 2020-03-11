e13a891ebaa83912d0c06d43bfd4767958a740e4
Ted Scharff <teds@3scan.com>
2020-03-11 14:52:58 -0700

    Option to suppy base64-encoded node configuration

--- opcua_exporter/opca_exporter_test.go
+++ opcua_exporter/opca_exporter_test.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestReadNodeFile(t *testing.T) {
-	testNodes := strings.NewReader(`[{"nodeName": "foo", "metricName": "bar"} ]`)
-	results, err := parseNodeJSONFile(testNodes)
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(results))
-	assert.IsType(t, Node{}, results[0])
-	assert.Equal(t, "foo", results[0].NodeName)
-	assert.Equal(t, "bar", results[0].MetricName)
+var testNodes = []Node{
+	Node{
+		NodeName:   "foo",
+		MetricName: "bar",
+	},
+}
 
-	results, err = parseNodeJSONFile(strings.NewReader("foooob not valid json here"))
+func TestReadNodeFile(t *testing.T) {
+	//testNodes := strings.NewReader(`[{"nodeName": "foo", "metricName": "bar"} ]`)
+	data, _ := json.Marshal(testNodes)
+	results, err := parseConfigJSON(bytes.NewReader(data))
+	assert.NoError(t, err)
+	assert.Equal(t, len(testNodes), len(results))
+	assert.IsType(t, Node{}, results[0])
+	assert.Equal(t, testNodes[0].NodeName, results[0].NodeName)
+	assert.Equal(t, testNodes[0].MetricName, results[0].MetricName)
+
+	results, err = parseConfigJSON(strings.NewReader("foooob not valid json here"))
 	assert.Error(t, err)
 	assert.Empty(t, results)
+}
+
+func TestB64Config(t *testing.T) {
+	data, _ := json.Marshal(testNodes)
+	encodedData := base64.StdEncoding.EncodeToString(data)
+	results, err := readConfigBase64(&encodedData)
+	assert.NoError(t, err)
+	assert.Equal(t, len(testNodes), len(results))
+	assert.IsType(t, Node{}, results[0])
+	assert.Equal(t, testNodes[0].NodeName, results[0].NodeName)
+	assert.Equal(t, testNodes[0].MetricName, results[0].MetricName)
 }
